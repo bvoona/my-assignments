b90aa26cc2708358aa487a840a15c28d4a33e8b1
bvoona <115697446+bvoona@users.noreply.github.com>
2022-11-07 00:03:23 +0530

    Add files via upload

---
+++ ExceptionHandling/src/com/ExceptionHandling/ArithmeticExpression.java
@@ -0,0 +1,16 @@
+package com.ExceptionHandling;
+
+public class ArithmeticExpression {
+    public static void main(String[] args){
+        int num1 = 6,num2 = 0, results;
+        try
+        {
+            results = num1/num2;
+            System.out.println("the result is" + results);
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println("can't be divided by zero"+e);
+        }
+    }
+}
